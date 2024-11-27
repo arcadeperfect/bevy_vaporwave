7e4e119b53272ffcd0b34ef6830ef739b6784f9c
arcadeperfect <alexander.harding@gmail.com>
2024-11-26 22:00:40 -0500

    post process build script

--- build_wasm.sh
+++ build_wasm.sh
@@ -2,3 +2,6 @@
 set -e 
 cargo build --release --target wasm32-unknown-unknown
 wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/release/bevy_vaporwave.wasm
+
+sed -i 's/getObject(arg0).focus();/const scrollPos = window.scrollY; getObject(arg0).focus(); window.scrollTo(0, scrollPos);/' ./out/bevy_vaporwave.js
+
