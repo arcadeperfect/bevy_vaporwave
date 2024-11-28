44db15a41800f27d168f06e6b1163c92d36cd567
arcadeperfect <alexander.harding@gmail.com>
2024-11-28 03:42:50 +0000

    Deploying to gh-pages from @ arcadeperfect/bevy_vaporwave@9b9b596d96ba0ce2d0184fa8d9d3b93c11e9118a 🚀

--- fix_js.sh
+++
@@ -1,8 +0,0 @@
-# Detect OS and use appropriate sed command
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    # macOS
-    sed -i '' 's/getObject(arg0).focus();/const scrollPos = window.scrollY; getObject(arg0).focus(); window.scrollTo(0, scrollPos);/' ./out/bevy_vaporwave.js
-else
-    # Linux and others
-    sed -i 's/getObject(arg0).focus();/const scrollPos = window.scrollY; getObject(arg0).focus(); window.scrollTo(0, scrollPos);/' ./out/bevy_vaporwave.js
-fi
